9801ca2c86d802e48ab0a419b62beca912cb9002
Olga Ivanova <snorra.ivanova@gmail.com>
2017-08-21 17:23:29 +0200

    Fix unicorn setting

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,7 +4,6 @@ pid_file = "#{deploy_to}/shared/tmp/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/tmp/unicorn.sock"
 log_file = "#{rack_root}/log/unicorn.log"
 err_log = "#{rack_root}/log/unicorn_error.log"
-old_pid = pid_file + '.oldbin'
 
 timeout 30
 worker_processes 2
@@ -13,18 +12,6 @@ listen socket_file, backlog: 1024
 pid pid_file
 stderr_path err_log
 stdout_path log_file
-
-before_fork do |server, worker|
-  # zero downtime deploy magic:
-  # if unicorn is already running, ask it to start a new process and quit.
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # already done
-    end
-  end
-end
 
 # Force the bundler gemfile environment variable to
 # reference the capistrano "current" symlink
