ff004dedc0fd1aea013a35affeef1f8030562aa6
Olga Ivanova <snorra.ivanova@gmail.com>
2017-08-21 16:32:47 +0200

    Update unicorn and deploy configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,39 +12,22 @@ set :default_environment, {
 set :pty, true
 set :ssh_options, forward_agent: true
 
+set :linked_files, fetch(:linked_files, []) + %w(
+  .env
+)
 server 'dev.datacrafts.io', user: 'crafter', port: 22032, roles: %w{app web db}
+
+set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
 
 set :rvm_type, :auto
 set :log_level, :debug
 
-set :linked_files, fetch(:linked_files, []) + %w(
-  .env
-)
-
 after 'deploy:publishing', 'deploy:restart'
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
 
 namespace :deploy do
 
   task :restart do
-    invoke 'puma:restart'
+    invoke 'unicorn:reload'
   end
 
 end
