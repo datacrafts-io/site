b599241e29dbdddd38de5e2d7faf4b64cac9e0a0
Andrey Kurashev <andreykurashev@gmail.com>
2018-08-25 20:05:01 +0600

    Show flash message after form submit

--- Gemfile
+++ Gemfile
@@ -2,19 +2,17 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-asset-pipeline'
+gem 'sinatra-flash'
 gem 'slim'
 
 gem 'bootstrap', '~> 4.1.1'
 
+gem 'puma'
+
 group :development do
-  gem 'thin'
   gem 'capistrano', require: false
   gem 'capistrano-bundler', require: false
   gem 'capistrano-rvm', require: false
   gem 'capistrano3-puma', require: false
   gem 'sshkit-sudo', require: false
-end
-
-group :production do
-  gem 'puma'
 end
--- landing_app.rb
+++ landing_app.rb
@@ -1,15 +1,22 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/asset_pipeline'
+require 'sinatra/flash'
 require 'bootstrap'
 
-enable :logging
-
 class LandingApp < Sinatra::Base
+  enable :sessions
+
   set :assets_paths, %w(assets assets/stylesheets assets/javascripts assets/images)
 
   register Sinatra::AssetPipeline
+  register Sinatra::Flash
 
   get '/' do
     slim :index
+  end
+
+  post '/' do
+    flash[:success] = "Thanks! We'll contact you ASAP."
+    redirect '/'
   end
 end
